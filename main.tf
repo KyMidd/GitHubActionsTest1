e0fe9b54d25ad9687ed6811413ba60dadcaeea9c
Kyler Middleton <kymidd@gmail.com>
2019-11-10 17:34:52 -0600

    fix route table

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_route_table" "route_table1" {
 
 # Build route table 2
 resource "aws_route_table" "route_table2" {
-  vpc_id = aws_vpc.vpc2.id
+  vpc_id = aws_vpc.vpc.id
 
   tags = {
     Name = "RouteTable2"
