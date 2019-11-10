422d11fc7e0f8fc7220275aba7266a3e34acfd80
Kyler Middleton <kymidd@gmail.com>
2019-11-10 17:40:02 -0600

    fix terraform ver req

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Require TF version to most recent
 terraform {
-  required_version = "=0.12.8"
+  required_version = "~> 0.12.8"
 }
 
 # Download any stable version in AWS provider of 2.19.0 or higher in 2.19 train
