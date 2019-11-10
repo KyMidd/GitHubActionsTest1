cd10c27ec343cf6aa7d15c54ae7dfdc2fdb61e7b
Kyler Middleton <kymidd@gmail.com>
2019-11-10 14:01:24 -0600

    updating aws provider

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ terraform {
 
 # Download any stable version in AWS provider of 2.19.0 or higher in 2.19 train
 provider "aws" {
-  region  = "us-east-1"
   version = "~> 2.27.0"
+  region  = "us-east-1"
 }
 
 # Build the VPC
