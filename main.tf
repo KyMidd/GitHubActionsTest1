6fb97be3ac066dfcfc5efb1cd206030eee3ba369
Kyler Middleton <kymidd@gmail.com>
2019-11-17 14:55:37 -0700

    add bootstrap, update plan

--- main.tf
+++ main.tf
@@ -1,20 +1,34 @@
-# Require TF version to most recent
+# Require TF version to be same as or greater than 0.12.13
 terraform {
-  required_version = "~> 0.12.8"
-#    backend "s3" {
-#    bucket         = "kyler-github-actions-demo-terraform-tfstate"
-#    key            = "terraform.tfstate"
-#    region         = "us-east-1"
-#    dynamodb_table = "aws-locks"
-#    encrypt        = true
-#  }
+  required_version = ">=0.12.13"
+  backend "s3" {
+    bucket         = "kyler-github-actions-demo-terraform-tfstate"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "aws-locks"
+    encrypt        = true
+  }
 }
 
-# Download any stable version in AWS provider of 2.19.0 or higher in 2.19 train
+# Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
 provider "aws" {
-  version = "~> 2.27.0"
   region  = "us-east-1"
+  version = "~> 2.36.0"
 }
+
+# Call the seed_module to build our ADO seed info
+module "bootstrap" {
+  source                      = "./modules/bootstrap"
+  name_of_s3_bucket           = "kyler-github-actions-demo-terraform-tfstate"
+  dynamo_db_table_name        = "aws-locks"
+  iam_user_name               = "GitHubActionsIamUser"
+  ado_iam_role_name           = "GitHubActionsIamRole"
+  aws_iam_policy_permits_name = "GitHubActionsIamPolicyPermits"
+  aws_iam_policy_assume_name  = "GitHubActionsIamPolicyAssume"
+}
+
+
+/* Commented out until after bootstrap
 
 # Build the VPC
 resource "aws_vpc" "vpc" {
@@ -46,3 +60,4 @@ resource "aws_route_table" "route_table2" {
     Terraform = "true"
   }
 }
+*/
