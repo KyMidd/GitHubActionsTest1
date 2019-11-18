409271df0b9656720257a49ccc204e86f6650e8d
Kyler Middleton <kymidd@gmail.com>
2019-11-17 17:07:24 -0700

    Reverting to copyable state

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 # Require TF version to be same as or greater than 0.12.13
 terraform {
   required_version = ">=0.12.13"
-  backend "s3" {
-    bucket         = "kyler-github-actions-demo-terraform-tfstate"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "aws-locks"
-    encrypt        = true
-  }
+  #backend "s3" {
+  #  bucket         = "kyler-github-actions-demo-terraform-tfstate"
+  #  key            = "terraform.tfstate"
+  #  region         = "us-east-1"
+  #  dynamodb_table = "aws-locks"
+  #  encrypt        = true
+  #}
 }
 
 # Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
@@ -15,6 +15,9 @@ provider "aws" {
   region  = "us-east-1"
   version = "~> 2.36.0"
 }
+
+
+/* Commented out until after bootstrap
 
 # Call the seed_module to build our ADO seed info
 module "bootstrap" {
