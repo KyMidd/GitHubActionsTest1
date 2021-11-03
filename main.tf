3d292b350b5e2296f16ca03faaea25c2d624ebcf
prateek-paatni <prateek.paatni@gmail.com>
2021-11-03 18:05:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,10 @@
 # Require TF version to be same as or greater than 0.12.13
 terraform {
-  required_version = ">=0.12.13"
-  #backend "s3" {
-  #  bucket         = "kyler-github-actions-demo-terraform-tfstate"
-  #  key            = "terraform.tfstate"
-  #  region         = "us-east-1"
-  #  dynamodb_table = "aws-locks"
-  #  encrypt        = true
-  #}
 }
 
 # Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
 provider "aws" {
   region  = "us-east-1"
-  version = "~> 2.36.0"
 }
 
 
